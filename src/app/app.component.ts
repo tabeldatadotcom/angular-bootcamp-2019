ee25e0f9bd7d6292514fb6b84cf874c719645554
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-18 14:25:18 +0700

    memampilkan data dengan *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'belajar-angular';
+  title: string = 'Belajar Angular6';
+
+  listMakanan: Array<string> = ['Pitza', 'Rendang'];
 }
