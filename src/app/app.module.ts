4de3965b0de768e7cda507e086799bf66668224f
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-19 10:30:49 +0700

    update reactive component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { CollectionComponent } from './list/collection/collection.component';
+import { FormReactiveComponent } from './form-reactive/form-reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    CollectionComponent
+    CollectionComponent,
+    FormReactiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-reactive/form-reactive.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-reactive',
+  templateUrl: './form-reactive.component.html',
+  styleUrls: ['./form-reactive.component.css']
+})
+export class FormReactiveComponent implements OnInit {
+
+  formGroup: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      'id': new FormControl(),
+      'nama': new FormControl()
+    });
+  }
+
+  handler(event: any){
+    console.log(this.formGroup.value);
+  }
+
+}
