1cfaec6cd609a4890548ebabfaf6c377448178a9
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-19 11:14:17 +0700

    update form arrray

--- src/app/form-reactive/form-reactive.component.ts
+++ src/app/form-reactive/form-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactive',
@@ -10,19 +10,30 @@ export class FormReactiveComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.formGroup = new FormGroup({
-      'id': new FormControl(),
-      'nama': new FormControl(),
-      'alamat': new FormGroup({
-        'kota': new FormControl(),
-        'rt': new FormControl(),
-        'rw': new FormControl(),
-        'jalan': new FormControl()
-      })
+      'id': this.formBuilder.control(''),
+      'nama': this.formBuilder.control(''),
+      'alamat': this.formBuilder.group({
+        'kota': this.formBuilder.control(''),
+        'rt': this.formBuilder.control(''),
+        'rw': this.formBuilder.control(''),
+        'jalan': this.formBuilder.control('')
+      }),
+      'hobi': this.formBuilder.array([
+       this.formBuilder.control('')
+      ])
     });
+  }
+
+  addHobi(){
+    this.hobi.push(this.formBuilder.control(''));
+  }
+
+  get hobi() : FormArray{
+    return this.formGroup.get('hobi') as FormArray;
   }
 
   handler(event: any){
