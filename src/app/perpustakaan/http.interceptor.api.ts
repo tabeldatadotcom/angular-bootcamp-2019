6532b59016f34d73b2cc190087c571f1aa4bcd57
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-04-02 14:25:57 +0700

    fixing auth interceptor

--- src/app/perpustakaan/http.interceptor.api.ts
+++ src/app/perpustakaan/http.interceptor.api.ts
@@ -13,13 +13,16 @@ export class HttpInterceptorApi implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this._loginService.getToken()}`
-      }
-    });
-    return next.handle(req);
+    if (!req.url.match('\/(oauth)\/[a-zA-Z0-9.?=&&_-]*')) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this._loginService.getToken()}`
+        }
+      });
+      return next.handle(req);
+    } else {
+      return next.handle(req.clone());
+    }
   }
 
 }
